73bdede7d9eb126720875c34468adf9cc4408ed8
James_G <jamesg.chef@gmail.com>
2021-03-18 11:32:44 +0000

    Added code to lib/what_should_i_eat.rb

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -4,5 +4,15 @@ require_relative "what_should_i_eat/version"
 
 module WhatShouldIEat
   class Error < StandardError; end
-  # Your code goes here...
+  
+  require "http"
+  result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
+
+  recipes = JSON.parse(result)
+
+  puts "For tonight's dinner we shall have #{recipes.first['items'].first['label']}"
+  puts "Find the recipe at #{recipes.first['items'].first['url']}"
+
+
+
 end
